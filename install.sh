7f19342404480ebe87ad215499416ccc51bde050
Gregwar <gregwar@bless.(none)>
2010-08-26 19:36:34 +0200

    Configuration bash+vim

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installation de la configuration VIM"
+cp vim/.vimrc ~/.vimrc
+
+echo "Installation de la configuration BASH"
+cp bash/.bashrc ~/.bashrc
---
+++ vim/.vimrc
@@ -0,0 +1,5 @@
+syn on
+set shiftwidth=4
+set smarttab
+set number
+filetype indent on
