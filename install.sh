2737624be389484d371d98e920aad8c519d862bf
Gregwar <g.passault@gmail.com>
2010-08-27 16:54:12 +0200

    Config gedit

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ cp vim/.vimrc ~/.vimrc
 
 echo "Installation de la configuration BASH"
 cp bash/.bashrc ~/.bashrc
+
+echo "Installation de la configuration GEDIT"
+cp "gregwar.xml" ~/.gnome2/gedit/styles/
+cp "gedit-2" ~/.gconf/apps/
