35c03033f408e8b8c35e8f77377100dd650ee008
Gregwar <g.passault@gmail.com>
2012-12-04 10:45:11 +0100

    [git] Adding git user & email

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ cp -R vim/.vim ~/.vim
 
 echo "Installation de la configuration BASH"
 cp bash/.bashrc ~/.bashrc
+
+echo "Configuring git"
+git config --global user.name "Gregwar"
+git config --global user.email "g.passault@gmail.com"
