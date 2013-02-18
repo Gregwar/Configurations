775e6f67cb8e591cd6b7b82ffa89cdec02255db6
Gregwar <g.passault@gmail.com>
2013-02-18 14:57:19 +0100

    Fix

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 echo "Installation de la configuration VIM"
 cp vim/.vimrc ~/.vimrc
-cp -R vim/.vim ~/.vim
+mkdir ~/.vim 2> /dev/null
+cp -R vim/.vim/* ~/.vim/
 
 echo "Installation de la configuration BASH"
 cp bash/.bashrc ~/.bashrc
