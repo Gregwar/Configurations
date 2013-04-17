117275ce6c6338c7fa5ca45b79d57902cd96b920
root <root@fitpc.(none)>
2013-04-17 16:27:56 +0200

    Adding rhino

--- apt-debian.sh
+++ apt-debian.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install iceweasel git vim curl php5-cli cu screen g++ gcc make cmake python python3 wireshark htop irb dfu-util python-serial php5-gd arp-scan
+sudo apt-get install iceweasel git vim curl php5-cli cu screen g++ gcc make cmake python python3 wireshark htop irb dfu-util python-serial php5-gd arp-scan rhino
